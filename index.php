5d8c1e5d831a610477dfd95459b5c267d3be4b92
AvoDjan <119239831+AvoDjan@users.noreply.github.com>
2023-03-01 16:02:49 +0100

    If...elseif...else... elseif et les switchs

--- index.php
+++ index.php
@@ -1,82 +1,187 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Ma page web</title>
-    </head>
-    <body>
-        <h1>Ma page web</h1>
-        <!-- echo date ('d/m/Y h:i:s') permet d'indiquer l'heure sur la page html (elle se régénère toute seule) -->
-        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
 
-        <?php
-            $userAge = 17; // La variable est créée et vaut 17
-            $userAge = 23; // La variable est modifiée et vaut 23
-            $userAge = 55; // La variable est modifiée et vaut 55
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page web</title>
+</head>
 
+<body>
+    <?php
+    $isEnabled = true; // La condition d'accès
 
-            //String
-            $fullname = "Grégory Yéramian";
-            $email = 'gregory.yeramian.devweb@exemple.com';
-
-            echo "Bonjour $fullname et bienvenue sur le site ! <br >";
-
-
-            // int (nombre entier)
-            $userAge = 17;
+    if ($isEnabled == true) {
+        echo "Vous êtes autorisé(e) à accéder au site ✅";
+    } else {
+        echo "Accès refusé ❌ ";
+    }
 
 
 
-            //float (nombre décimal)
-            $price = 57.3;
+    $isAllowedToEnter = "Oui";
+
+    // SI on a l'autorisation d'entrer
+    if ($isAllowedToEnter == "Oui") {
+        // instructions à exécuter quand on est autorisé à entrer
+        // SINON SI on n'a pas l'autorisation d'entrer
+    } elseif ($isAllowedToEnter == "Non") {
+        // instructions à exécuter quand on n'est pas autorisé à entrer
+    }   // SINON (la variable ne contient ni Oui ni Non, on ne peut pas agir)
+    else {
+        echo "Euh, je ne comprends pas ton choix, tu peux me le rappeler s'il te plaît ?";
+    }
 
 
 
-            //Bool (booléen)
+    $isAllowedToEnter = true;
 
-            $isAuthor = true;
-            $isAdministrator = false;
-
-
-            // NULL
-
-            $noValue = NULL;
-
-            $fullname = "Grégory Yéramian"; // String pour le prénom nom
-            $userAge = 27; // l'âge 
-            $userVille = "Tours"; // string pour la ville
-
-            // Ouvrir des guillemets à chaque fois qu'on les referment pour le . $Variable .
-            echo 'Bonjour ' . $fullname . ' vous avez ' . $userAge . ' ans et vous habitez à ' . $userVille . ' bienvenue à vous sur le site !';
+    // Si pas autorisé
+    if (!$isAllowedToEnter) { // Le ! permet de dire qu'il est différent du "true" plus haut. On peut aussi mettre if($isAllowedToEnter == false)
+    }
 
 
+    $isEnabled = true;
+    $isOwner = false;
+    $isAdmin = true;
 
-            // (Les chaines de caractères) string = nom qui contient texte
-            // (les nombres entiers) int = les nombres du type 1,2,3 ou -1,-2,-3
-            // (Les nombres décimaux) float = Les nombres à virgules comme 14,738
-            // (Les booléens) bool = un type qui stock vrai ou faux
-            // (Rien) NULL = dire qu'une variable ne contient rien (l'absence de type)
+    // && = et / || = ou
 
-
-            $number = 2 + 4; // $number prend la valeur 6
-            $number = 5 - 1; // $number prend la valeur 4
-            $number = 3 * 5; // $number prend la valeur 15
-            $number = 10 / 2; // $number prend la valeur 5
-
-
-            // Allez on rajoute un peu de difficulté
-            $number = 3 * 5 + 1; // $number prend la valeur 16
-            $number = (1 + 2) * 2; // $number prend la valeur 6
+    if (($isEnabled && $isOwner) || $isAdmin) {
+        echo 'Accès à la recette validé ✅';
+    } else {
+        echo 'Accès à la recette interdit ! ❌';
+    }
 
 
-            $number = 10;
-            $result = ($number + 5) * $number; // $result prend la valeur 150
 
-            
-            $number = 10 % 5; // $number prend la valeur 0 car la division tombe juste
-            $number = 10 % 3; // $number prend la valeur 1 car il reste 1
+    $chickenRecipesEnabled = true;
+
+    if ($chickenRecipesEnabled) {
+        echo '<h1>Liste des recettes à base de poulet</h1>';
+    }
+
+    // Ou alors un autre syntaxe alternative
+    /* <?php $chickenRecipesEnabled = true; ?>
+
+       <?php if ($chickenRecipesEnabled): ?> <!-- Ne pas oublier le ":" -->
+
+       <h1>Liste des recettes à base de poulet</h1>
+
+       <?php endif; ?><!-- Ni le ";" après le endif --> */
 
 
-        ?>
-    </body>
+       $grade = 16;
+
+    if ($grade == 0) 
+    {
+        echo "Tu es vraiment un gros nul !!!";
+    } 
+    elseif ($grade == 5) 
+    {
+        echo "Tu es très mauvais";
+    }
+     elseif ($grade == 7) 
+    {
+        echo "Tu es mauvais";
+    }
+     elseif ($grade == 10) 
+    {
+        echo "Tu as pile poil la moyenne, c'est un peu juste…";
+    }
+     elseif ($grade == 12) 
+    {
+        echo "Tu es assez bon";
+    } 
+    elseif ($grade == 16) 
+    {
+        echo "Tu te débrouilles très bien !";
+    }
+    elseif ($grade == 20) 
+    {
+        echo "Excellent travail, c'est parfait !";
+    }
+    else 
+    {
+        echo "Désolé, je n'ai pas de message à afficher pour cette note";
+    }
+
+    // On peut aussi l'écrire d'une autre façon qui revient au même
+
+    /* switch ($grade) // on indique sur quelle variable on travaille
+{ 
+    case 0: // dans le cas où $grade vaut 0
+        echo "Tu es vraiment un gros nul !!!";
+    break;
+    
+    case 5: // dans le cas où $grade vaut 5
+        echo "Tu es très mauvais";
+    break;
+    
+    case 7: // dans le cas où $grade vaut 7
+        echo "Tu es mauvais";
+    break;
+    
+    case 10: // etc. etc.
+        echo "Tu as pile poil la moyenne, c'est un peu juste…";
+    break;
+    
+    case 12:
+        echo "Tu es assez bon";
+    break;
+    
+    case 16:
+        echo "Tu te débrouilles très bien !";
+    break;
+    
+    case 20:
+        echo "Excellent travail, c'est parfait !";
+    break;
+    
+    default:
+        echo "Désolé, je n'ai pas de message à afficher pour cette note";
+} */
+
+
+    $note = 12;
+
+    switch ($note)
+    {
+        case 0:
+            echo "Tu es merdique";
+        break;
+
+        case 5:
+            echo "Tu es naze";
+        break;
+
+        case 7:
+            echo "Tu n'as pas la moyenne, poursuit tes efforts";
+        break;
+
+        case 10:
+            echo "Tu as pile poil la moyenne, attention la prochaine fois";
+        break;
+
+        case 12:
+            echo "Tu as un peu plus que la moyenne, continues tes efforts";
+        break;
+
+        case 16:
+            echo "Bonne note, bon projet";
+        break;
+
+        case 20:
+            echo "Excellente note, travail bien fournis";
+        break;
+
+        default:
+            echo "Désolé, je ne reconnais pas la note, recommence stp";
+    }
+
+
+
+
+    ?>
+</body>
+
 </html>
